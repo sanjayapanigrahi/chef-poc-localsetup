482d8476fe3736e267a11eca4062203f391e74d4
SanjayaPanigrahi <sanjayp.panigrahi@gmail.com>
2022-01-05 17:59:43 +0530

    Updates on Script

---
+++ scripts/install-chef-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#wget https://packages.chef.io/files/stable/chef-server/14.10.23/ubuntu/20.04/chef-server-core_14.10.23-1_amd64.deb
+#sudo dpkg -i chef-server-core_14.10.23-1_amd64.deb
+#sudo chef-server-ctl reconfigure --chef-license=accept
+#sudo chef-server-ctl install chef-manage
+#sudo chef-server-ctl reconfigure
+#sudo chef-manage-ctl reconfigure
+#sudo chef-server-ctl user-create demo-user sanjaya panigrahi sanjaya.panigrahi@ibm.com 'demo-user' --filename demo-user.pem
+#sudo chef-server-ctl org-create  demo-org "Demo Organisation" --association_user demo-user --filename demo-org.pem
